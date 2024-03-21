10238dd349c3fb7aae7d72d20a67467395a5cede
Matheus Castiglioni <mahenrique94@users.noreply.github.com>
2024-03-21 09:31:59 -0300

    Tabs

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Vim package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
@@ -13,10 +14,38 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+-- Installing all plugins under plugins folder automatically
 require("lazy").setup("plugins", opts)
 
+-- Global settings
 require("config")
+
+-- Keymaps/Shortcuts
 require("config/keymaps")
 
+-- Status line
 require('lualine').setup()
+
+-- Tabs
+require("bufferline").setup()
+
+-- Noice
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+})
 
